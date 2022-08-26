96a1986ffa1e8a9e572e2c5ebbff5141c26744a3
Kevin Bell <bellKevin@pm.me>
2022-08-26 10:06:35 -0600

    Update Rectangle.java

--- rectangle/src/rectangle/Rectangle.java
+++ rectangle/src/rectangle/Rectangle.java
@@ -1,9 +1,9 @@
 package rectangle;
-import java.util.Scanner;
+
 public class Rectangle {
 
     public static void main(String[] args) {
-        System.out.println("Ch 6 Rectangle Project by Kevin Bell\n");
+        System.out.println("Ch 6 Rectangle Project by Kevin Bell \n");
         rectangleClass paper;
         paper = new rectangleClass();
         paper.setLength();
@@ -12,5 +12,5 @@ public class Rectangle {
         paper.getPerimeter();
         paper.getArea();
         paper.printRectangleData();
-    }
-}
+    } // end main method
+} // end class rectangle
