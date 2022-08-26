42ebdc1fc4d2ea1dd13a7985b4c3008f1653cc6f
Kevin Bell <bellKevin@pm.me>
2022-08-26 10:18:50 -0600

    Update Rectangle.java

--- rectangle/src/rectangle/Rectangle.java
+++ rectangle/src/rectangle/Rectangle.java
@@ -4,8 +4,7 @@ public class Rectangle {
 
     public static void main(String[] args) {
         System.out.println("Ch 6 Rectangle Project by Kevin Bell \n");
-        rectangleClass paper;
-        paper = new rectangleClass();
+        rectangleClass paper = new rectangleClass();
         paper.setLength();
         paper.setWidth();
         paper.isSquare();
