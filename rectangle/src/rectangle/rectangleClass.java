3496494bc64c72d3909502d601193f5adeb6a92a
Kevin Bell <bellKevin@pm.me>
2022-08-26 10:11:43 -0600

    Update rectangleClass.java

--- rectangle/src/rectangle/rectangleClass.java
+++ rectangle/src/rectangle/rectangleClass.java
@@ -9,29 +9,29 @@ public class rectangleClass {
 
     public int getPerimeter() {
         return this.perimeter;
-    }
+    } // end accessor method
 
     public int getArea() {
         return this.area;
-    }
+    }// end accessor method
 
     public int getLenth() {
         return this.length;
-    }
+    }// end accessor method
 
     public int getWidth() {
         return this.width;
-    }
+    }// end accessor method
 
     void setLength() {
         System.out.print("Please enter length: ");
         length = computerKeyboardInput.nextInt();
-    }
+    } // end mutator method
 
     void setWidth() {
         System.out.print("Please enter width: ");
         width = computerKeyboardInput.nextInt();
-    }
+    } // end mutator method
 
     public boolean isSquare() {
         if (length == width) {
@@ -40,13 +40,13 @@ public class rectangleClass {
         } else {
             System.out.println("This paper is a rectangle: " + this.length + " x " + this.width);
             return false;
-        }
-    }
+        } // end if/else condition
+    } // end isSquare method
 
     void printRectangleData() {
         perimeter = 2 * length + 2 * width;
         System.out.println("Perimeter: " + this.perimeter);
         area = length * width;
         System.out.print("Area: " + this.area + "\n");
-    }
-}
+    } // end printRectangleData method
+} // end rectangleClass class
